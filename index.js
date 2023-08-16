427e4201e3477abdbf94661817620d994184c912
heredialucas <heredialucasfac22@gmail.com>
2023-08-16 19:43:46 +1200

    Update init server

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-const PORT = procces.env.PORT ?? 3001;
+const PORT = process.env.PORT ?? 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
